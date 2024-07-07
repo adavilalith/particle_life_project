1871d4d03c1197209580a1726b3400d393032989
Adavi Lalith <adavilalith11@gmail.com>
2024-07-07 12:43:48 +0530

    creating basic windowing using awt and JFrame

---
+++ Partical Life/src/main/GamePanel.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.awt.*;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+    
+    final int originalTileSize = 16;
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.blue);
+        this.setDoubleBuffered(true);
+    }
+}
